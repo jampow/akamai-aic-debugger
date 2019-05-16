9df3e23ad14756b6896545474345b4014b5f0292
Gianpaulo M. Soares <jam_pow@hotmail.com>
2019-05-16 20:08:41 -0300

    feat: added secondary server

--- popup.js
+++ popup.js
@@ -1,36 +1,47 @@
-const url = 'https://dafitistatic-a.akamaihd.net/'
 
-function setCookie(value, url){
+function setRttCookie(value, url, domain) {
 	chrome.cookies.set({
 		'url': url,
 		'name': 'aic-rtt',
-		'domain': '.akamaihd.net',
+		'domain': domain,
 		'value': value
-	},function (cookie){
-		setSelected(value)
-	});
+	})
+}
+
+function setAkamaiCookie(value) {
+	setRttCookie(value, 'https://dafitistatic-a.akamaihd.net/', '.akamaihd.net')
+}
+
+function setDftStaticCookie(value) {
+	setRttCookie(value, 'https://t-static.dafiti.com.br/', '.dafiti.com.br')
 }
 
 function setSelected(rtt) {
-	document.querySelectorAll('li').forEach(function(el) { el.classList.remove('active') })
-	document.getElementById('rtt' + rtt).classList.add('active')
+	var name = 'active'
+	document.querySelectorAll('li').forEach(function(el) { el.classList.remove(name) })
+	document.getElementById('rtt' + rtt).classList.add(name)
+}
+
+function setCookie(rtt) {
+	setDftStaticCookie(rtt)
+	setAkamaiCookie(rtt)
+	setSelected(rtt)
 }
 
 chrome.cookies.get({
 	'name': 'aic-rtt',
-	'url': url
+	'url': 'https://t-static.dafiti.com.br/'
 },function(cookie) {
 	if (!cookie || !cookie.name) return
-	console.log('cookie', cookie)
 	setSelected(cookie.value)
 })
 
 var btn10 = document.getElementById('rtt10')
-btn10.addEventListener('click',function(){ setCookie('10', url) }, false)
+btn10.addEventListener('click',function(){ setCookie('10') }, false)
 
 var btn100 = document.getElementById('rtt100')
-btn100.addEventListener('click',function(){ setCookie('100', url) }, false)
+btn100.addEventListener('click',function(){ setCookie('100') }, false)
 
 var btn400 = document.getElementById('rtt400')
-btn400.addEventListener('click',function(){ setCookie('400', url) }, false)
+btn400.addEventListener('click',function(){ setCookie('400') }, false)
 
